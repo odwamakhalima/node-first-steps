641597bf1c5bcf48c3dc2ef6d312df3fb0d24124
Odwa Makhalima <3652687@myuwc.ac.za>
2019-09-09 16:15:25 +0200

    final commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
+var figlet = require('figlet');
 
 const greet = require('./greet');
 
-const styledMessage = chalk.bgYellow.green(greet('Xola'));
-console.log(styledMessage)
+
+
+figlet(greet('xola'), function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    const styledMessage = chalk.bgBlack.yellow(data);
+    console.log(styledMessage)
+});
